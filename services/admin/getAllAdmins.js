4024ab6b8236b6fb23209331434906c9ee6ac0e0
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-11-06 18:06:16 +0530

    resolved some error

--- services/admin/getAllAdmins.js
+++ services/admin/getAllAdmins.js
@@ -1,7 +1,7 @@
 const User = require("../../models/User");
 
 async function getAllAdmins() {
-    const admins = await User.find({ role: "admin" }, "username _id")
+    const admins = await User.find({ role: "admin" }, "username _id phone email")
     return admins;
 }
 
