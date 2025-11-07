7aea949725f0b2e5bc40f3e6ae1a517a141854ce
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-11-07 15:44:06 +0530

    resolved some issue

--- utils/jwt.js
+++ utils/jwt.js
@@ -7,7 +7,7 @@ function generateToken(payload, type = "access") {
 
     if (type === "access") {
         const access_token_secret = process.env.access_token_secret;
-        return jwt.sign(payload, access_token_secret, { expiresIn: '30m' });
+        return jwt.sign(payload, access_token_secret, { expiresIn: '1h' });
     } else if (type === "refresh") {
         const refresh_token_secret = process.env.refresh_token_secret;
         return jwt.sign(payload, refresh_token_secret, { expiresIn: '7d' });
