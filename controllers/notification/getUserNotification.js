2f8d5b41a7d66047ea2ebe1b0f89eae3d97b0d1b
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-11-18 16:15:56 +0530

    updated getuserpost with category filter

--- controllers/notification/getUserNotification.js
+++ controllers/notification/getUserNotification.js
@@ -4,8 +4,9 @@ const notificationService = require('../../services/notification');
 
 async function getUserNotification(req, res, next) {
     try {
-        const userId = req.user.id
-        const notifications = await notificationService.getUserNotification(userId);
+        const userId = req.user.id;
+        const categories = req.query['categories[]'] || [];
+        const notifications = await notificationService.getUserNotification(userId,categories);
         res.status(STATUS_CODES.OK).json({
             success: true,
             message: 'notifications fetched successfully',
--- services/notification/getUserNotification.js
+++ services/notification/getUserNotification.js
@@ -1,9 +1,13 @@
 const Notification = require('../../models/Notification');
 
-async function getUserNotification(userId) {
-    const notifications = await Notification.find({ user_id: userId }, `_id title description 
+async function getUserNotification(userId, categories) {
+    const notifications = await Notification.find({
+        user_id: userId,
+        ...(categories.length > 0 ? { category_names: { $in: categories } } : {})
+    }, `_id title description 
         is_snoozed is_active category_names next_notification_date notification_frequency  notify_before notify_before_unit notific_gap_unit frequency`)
         .sort({ createdAt: -1 });
+
     return notifications;
 }
 
