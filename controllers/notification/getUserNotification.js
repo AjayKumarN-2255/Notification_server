a3f84b958c643e82a2e4497e7c2391318a20fb56
AjayKumarN-2255 <kumar.ajay@midnay.com>
2025-11-19 13:01:00 +0530

    added from to filter in get user notification

--- controllers/notification/getUserNotification.js
+++ controllers/notification/getUserNotification.js
@@ -7,7 +7,9 @@ async function getUserNotification(req, res, next) {
         const userId = req.user.id;
         const categories = req.query['categories[]'] || [];
         const searchTerm = req.query.searchTerm;
-        const notifications = await notificationService.getUserNotification(userId, categories, searchTerm);
+        const from = req.query.from;
+        const to = req.query.to;
+        const notifications = await notificationService.getUserNotification(userId, categories, searchTerm, from, to);
         res.status(STATUS_CODES.OK).json({
             success: true,
             message: 'notifications fetched successfully',
--- services/notification/getUserNotification.js
+++ services/notification/getUserNotification.js
@@ -1,10 +1,25 @@
 const Notification = require('../../models/Notification');
 
-async function getUserNotification(userId, categories, searchTerm) {
+async function getUserNotification(userId, categories, searchTerm, from, to) {
+
+    let fromDate;
+    let toDate;
+    if (from) {
+        fromDate = new Date(from);
+        fromDate.setHours(0, 0, 0, 0);
+    }
+
+    if (to) {
+        toDate = new Date(to);
+        toDate.setHours(23, 59, 59, 999);
+    }
+
     const notifications = await Notification.find({
         user_id: userId,
         ...(categories.length > 0 ? { category_names: { $in: categories } } : {}),
         ...(searchTerm && searchTerm.trim() !== "" ? { title: { $regex: searchTerm.trim(), $options: "i" } } : {}),
+        ...(from ? { next_notification_date: { $gte: fromDate } } : {}),
+        ...(to ? { next_notification_date: { $lte: toDate } } : {}),
     },
         `_id title description 
         is_snoozed is_active category_names next_notification_date notification_frequency  notify_before notify_before_unit notific_gap_unit frequency`
